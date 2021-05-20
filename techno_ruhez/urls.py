c8be21914c5a73af1242d5e36243ba3f6ed87e19
Jashandeep singh <62203228+jashandeep1659@users.noreply.github.com>
2021-05-20 19:42:21 +0530

    Update urls.py

--- techno_ruhez/urls.py
+++ techno_ruhez/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls')),
+    path('a/', include('core.urls')),
     path('tinymce/', include('tinymce.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
